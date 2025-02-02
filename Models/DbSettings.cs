b39bf0ec85603bd922f88deead5696316ca11850
Khushbu <0109khushbu@gmail.com>
2025-02-01 21:15:03 -0500

    feat: restructure dir

--- Models/DbSettings.cs
+++
@@ -1,9 +0,0 @@
-//ToDoAPI/Models/DbSettings.cs
-
-namespace ToDoAPI.Models
-{
-    public class DbSettings
-    {
-        public required string ConnectionString { get; set; }
-    }
-}
--- Models/ErrorResponse.cs
+++
@@ -1,9 +0,0 @@
-namespace ToDoAPI.Models
-{
-    public class ErrorResponse
-    {
-        public required string Title { get; set; }
-        public int StatusCode { get; set; }
-        public required string Message { get; set; }
-    }
-}
